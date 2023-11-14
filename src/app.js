bceca4c990f2f36b2bf3067014fac513f90cae3d
Hass2an <hassankhan00914@gmail.com>
2023-11-14 07:15:46 +0500

    utils , api error ,api response ,

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.ORS_ORIGN,
+    credentials: true,
+    
+}));
+
+
+app.use(express.json({limit:"16kb"})); // Parses incoming JSON data in the request body with a limit of 16 kilobytes.
+app.use(express.urlencoded({extended : true,
+    limit:"16kb"})); // they upadete url with special characters
+app.use(express.static("public")); // Serves static files (e.g., PDFs and images) from the "public" directory to make them accessible via specific URLs.
+
+app.use(cookieParser()); //Parses and manages cookies sent by clients in HTTP requests for reading and setting cookies.
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import 'dotenv/config'
-
+import { app } from './app.js';
 import connectDB from "./db/index.js";
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is listening on ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Connection Failed:",err);
+})
